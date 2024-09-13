fe9f9c515e29531b90cff391fe322252b39125d9
Prabhanjan K.M. <prabhanjan1506@gmail.com>
2024-09-13 07:53:30 +0530

    old

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,16 @@ import Products from './pages/Products';
 import About from './pages/About';
 import Contact from './pages/Contact'
 
-
 function App() {
   return (
     <Router>
-      
-      
       <Routes>
         <Route path="/" element={<Home />} />
          <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} /> 
       </Routes> 
-   
     </Router>
-     
-     
   );
 }
 
